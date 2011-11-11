14052a2c970ea32d614c266d6254224dca77dda5
Jerzon Hernandez <joel.hz24@gmail.com>
2011-11-10 20:25:03 -0600

    probando merge

--- app/models/estreno.rb
+++ app/models/estreno.rb
@@ -1,8 +1,8 @@
 class Estreno < ActiveRecord::Base
-	validates_presence_of :titulo, :message => "El titulo no puede estar en blanco"
-	validates_uniqueness_of :titulo
-	has_attached_file :imagen, :styles => {
-	    				:thumb=> "100x150#",
-	    				:small  => "150x150>"}
+validates_presence_of :titulo, :message => "El titulo no puede estar en blanco"
+validates_uniqueness_of :titulo
+has_attached_file :imagen, :styles => {
+  :thumb=> "100x150#",
+  :small  => "150x150>"}
 	
 end
